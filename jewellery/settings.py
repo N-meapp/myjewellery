ca696aa81e9d23dcb8756e203a6fbca976149d2c
developersnme <developersnme@gmail.com>
2025-07-05 14:43:24 +0530

    check2

--- jewellery/settings.py
+++ jewellery/settings.py
@@ -39,8 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'jewelleryapp',
 
+    'jewelleryapp.apps.JewelleryappConfig', 
     'rest_framework',
     'rest_framework.authtoken',
     'dj_rest_auth',
